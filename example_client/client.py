044707c8497fac0892654897539a3e6a64ebc857
Michael Kennedy <michael@talkpython.fm>
2024-01-18 21:38:25 -0800

    better user agent, start of paging long responses.

--- example_client/client.py
+++ example_client/client.py
@@ -32,26 +32,5 @@ print(lst)
 subscribers = listmonk.subscribers()
 print(f'{len(subscribers):,} subscribers returned')
 print(subscribers[0])
-# websites = umami.websites()
-# print(f"Found {len(websites):,} websites.")
-# print("First website in list:")
-# print(websites[0])
-# print()
-#
-# if test_domain := settings.get('test_domain'):
-#
-#     test_site = [w for w in websites if w.domain == test_domain][0]
-#     print(f"Using {test_domain} for testing events.")
-#
-#     event_resp = umami.new_event(
-#         website_id=test_site.id,
-#         event_name='Umami-Test-Event3',
-#         title='Umami-Test-Event3',
-#         hostname=test_site.domain,
-#         url='/users/actions',
-#         custom_data={'client': 'umami-tester-v1'},
-#         referrer='https://talkpython.fm')
-#
-#     print(f"Created new event: {event_resp}")
-# else:
-#     print("No test domain, skipping event creation.")
+
+
