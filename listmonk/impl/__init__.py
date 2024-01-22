1e15854d564e2b23402244a84c3d9bc3c5f55a24
Michael Kennedy <michael@talkpython.fm>
2024-01-22 11:01:01 -0800

    Added doc strings for most data and methods.

--- listmonk/impl/__init__.py
+++ listmonk/impl/__init__.py
@@ -7,7 +7,9 @@ import httpx
 
 from listmonk import models, urls  # noqa: F401
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
+
+from listmonk.errors import ValidationError, OperationNotAllowedError
 
 from listmonk.models import SubscriberStatuses
 
@@ -16,9 +18,9 @@ url_base: Optional[str] = None
 username: Optional[str] = None
 password: Optional[str] = None
 
-user_agent = (f'Listmonk-Client v{__version__} / '
-              f'Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro} / '
-              f'{sys.platform.capitalize()}')
+user_agent: str = (f'Listmonk-Client v{__version__} / '
+                   f'Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro} / '
+                   f'{sys.platform.capitalize()}')
 
 core_headers: dict[str, Optional[str]] = {
     'Authorization': None,  # Set at login
@@ -32,6 +34,12 @@ core_headers: dict[str, Optional[str]] = {
 # region def get_base_url() -> Optional[str]
 
 def get_base_url() -> Optional[str]:
+    """
+    Each Listmonk instance lives somewhere. This is where yours lives.
+    For example, https://listmonk.somedomain.tech.
+
+    Returns: The base URL of your instance.
+    """
     return url_base
 
 
@@ -40,8 +48,22 @@ def get_base_url() -> Optional[str]:
 # region def set_url_base(url: str)
 
 def set_url_base(url: str):
+    """
+    Each Listmonk instance lives somewhere. This is where yours lives.
+    For example, https://listmonk.somedomain.tech.
+    Args:
+        url: The base URL of your instance without /api.
+    """
     if not url or not url.strip():
-        raise Exception("URL must not be empty")
+        raise ValidationError("URL must not be empty.")
+
+    # noinspection HttpUrlsUsage
+    if not url.startswith('http://') and not url.startswith('https://'):
+        # noinspection HttpUrlsUsage
+        raise ValidationError("The url must start with the HTTP scheme (http:// or https://).")
+
+    if url.endswith('/'):
+        url = url.rstrip('/')
 
     global url_base
     url_base = url.strip()
@@ -51,11 +73,20 @@ def set_url_base(url: str):
 
 # region def login(user_name: str, pw: str)
 
-def login(user_name: str, pw: str):
+def login(user_name: str, pw: str) -> bool:
+    """
+    Logs into Listmonk and stores that authentication for the life of your app.
+    Args:
+        user_name: Your Listmonk username
+        pw: Your Listmonk password
+
+    Returns: Returns a boolean indicating whether the login was successful.
+    """
+
     global core_headers, username, password
 
     if not url_base or not url_base.strip():
-        raise Exception("base_url must be set before you can call login.")
+        raise OperationNotAllowedError("base_url must be set before you can call login.")
 
     validate_login(user_name, pw)
     username = user_name
@@ -77,6 +108,10 @@ def login(user_name: str, pw: str):
 # region def lists() -> list[models.MailingList]
 
 def lists() -> list[models.MailingList]:
+    """
+    Get mailing lists on the server.
+    Returns: List of MailingList objects with the full details of that list.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -94,6 +129,12 @@ def lists() -> list[models.MailingList]:
 # region def list_by_id(list_id: int) -> Optional[models.MailingList]
 
 def list_by_id(list_id: int) -> Optional[models.MailingList]:
+    """
+    Get the full details of a list with the given ID.
+    Args:
+        list_id: A list to get the details about, e.g. 7.
+    Returns: MailingList object with the full details of a list.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -113,6 +154,13 @@ def list_by_id(list_id: int) -> Optional[models.MailingList]:
 # region def subscribers(query_text: Optional[str] = None, list_id: Optional[int] = None) -> list[models.Subscriber]
 
 def subscribers(query_text: Optional[str] = None, list_id: Optional[int] = None) -> list[models.Subscriber]:
+    """
+    Get a list of subscribers matching the criteria provided. If none, then all subscribers are returned.
+    Args:
+        query_text: Custom query text such as "subscribers.attribs->>'city' = 'Portland'". See the full documentation at https://listmonk.app/docs/querying-and-segmentation/
+        list_id: Pass a list ID and get the subscribers, matching the query, from that list.
+    Returns: A list of subscribers matching the criteria provided. If none, then all subscribers are returned.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -120,12 +168,12 @@ def subscribers(query_text: Optional[str] = None, list_id: Optional[int] = None)
     page_num = 1
     partial_results, more = _fragment_of_subscribers(page_num, list_id, query_text)
     raw_results.extend(partial_results)
-    print(f"subscribers(): Got {len(raw_results)} so far, more? {more}")
+    # Logging someday: print(f"subscribers(): Got {len(raw_results)} so far, more? {more}")
     while more:
         page_num += 1
         partial_results, more = _fragment_of_subscribers(page_num, list_id, query_text)
         raw_results.extend(partial_results)
-        print(f"subscribers(): Got {len(raw_results)} so far on page {page_num}, more? {more}")
+        # Logging someday: print(f"subscribers(): Got {len(raw_results)} so far on page {page_num}, more? {more}")
 
     subscriber_list = [models.Subscriber(**d) for d in raw_results]
 
@@ -139,6 +187,7 @@ def subscribers(query_text: Optional[str] = None, list_id: Optional[int] = None)
 def _fragment_of_subscribers(page_num: int, list_id: Optional[int], query_text: Optional[str]) \
         -> Tuple[list[dict], bool]:
     """
+    Internal use only.
     Returns:
         Tuple of partial_results, more_to_retrieve
     """
@@ -173,6 +222,12 @@ def _fragment_of_subscribers(page_num: int, list_id: Optional[int], query_text: 
 # region def subscriber_by_email(email: str) -> Optional[models.Subscriber]
 
 def subscriber_by_email(email: str) -> Optional[models.Subscriber]:
+    """
+    Retrieves the subscribe by email (e.g. "some_user@talkpython.fm")
+    Args:
+        email: Email of the subscriber (e.g. "some_user@talkpython.fm")
+    Returns: The subscribe if found, None otherwise.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -196,6 +251,12 @@ def subscriber_by_email(email: str) -> Optional[models.Subscriber]:
 # region def subscriber_by_id(subscriber_id: int) -> Optional[models.Subscriber]
 
 def subscriber_by_id(subscriber_id: int) -> Optional[models.Subscriber]:
+    """
+    Retrieves the subscribe by id (e.g. 201)
+    Args:
+        subscriber_id: ID of the subscriber (e.g. 201)
+    Returns: The subscribe if found, None otherwise.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -218,6 +279,12 @@ def subscriber_by_id(subscriber_id: int) -> Optional[models.Subscriber]:
 # region subscriber_by_uuid(subscriber_uuid: str) -> Optional[models.Subscriber]
 
 def subscriber_by_uuid(subscriber_uuid: str) -> Optional[models.Subscriber]:
+    """
+    Retrieves the subscribe by uuid (e.g. "c37786af-e6ab-4260-9b49-740adbecb6ed")
+    Args:
+        subscriber_uuid: UUID of the subscriber (e.g. "c37786af-e6ab-4260-9b49-740aaaa6ed")
+    Returns: The subscribe if found, None otherwise.
+    """
     global core_headers
     validate_state(url=True, user=True)
 
@@ -238,10 +305,19 @@ def subscriber_by_uuid(subscriber_uuid: str) -> Optional[models.Subscriber]:
 # endregion
 
 # region def create_subscriber(email: str, name: str, list_ids: set[int], pre_confirm: bool, attribs: dict)
-# -> models.Subscriber
 
 def create_subscriber(email: str, name: str, list_ids: set[int],
                       pre_confirm: bool, attribs: dict) -> models.Subscriber:
+    """
+    Create a new subscriber on the Listmonk server.
+    Args:
+        email: Email of the subscriber.
+        name: Full name (first[SPACE]last) of the subscriber
+        list_ids: List of list IDs for the lists to add them to (say that 3 times fast!)
+        pre_confirm: Whether to preconfirm the subscriber for double opt-in lists (no email to them)
+        attribs: Custom dictionary for the attribs data on the user record (queryable in the subscriber UI).
+    Returns: The Subscribe object that was created on the server with ID, UUID, and much more.
+    """
     global core_headers
     validate_state(url=True, user=True)
     email = (email or '').lower().strip()
@@ -269,6 +345,14 @@ def create_subscriber(email: str, name: str, list_ids: set[int],
 # region def delete_subscriber(email: Optional[str] = None, overriding_subscriber_id: Optional[int] = None) -> bool
 
 def delete_subscriber(email: Optional[str] = None, overriding_subscriber_id: Optional[int] = None) -> bool:
+    """
+    Completely delete a subscriber from your system (it's as if they never were there).
+    If your goal is to unsubscribe them, then use the block_subscriber method.
+    Args:
+        email: Email of the account to delete.
+        overriding_subscriber_id:  Optional ID of the account to delete (takes precedence).
+    Returns: True if they were successfully deleted, False otherwise.
+    """
     global core_headers
     validate_state(url=True, user=True)
     email = (email or '').lower().strip()
@@ -292,10 +376,18 @@ def delete_subscriber(email: Optional[str] = None, overriding_subscriber_id: Opt
 
 # endregion
 
-
 # region def confirm_optin(subscriber_uuid: str, list_uuid: str) -> bool
 
 def confirm_optin(subscriber_uuid: str, list_uuid: str) -> bool:
+    """
+    For opt-in situations, subscribers are added as unconfirmed first. This method will opt them in
+    via the API. You should only do this when they are actually opting in. If you have your own opt-in
+    form but it's via your code, then this makes sense.
+    Args:
+        subscriber_uuid: The Subscriber.uuid value for the subscriber.
+        list_uuid: The MailingList.uuid value for the list.
+    Returns: True if they were successfully opted in.
+    """
     global core_headers
     validate_state(url=True, user=True)
     if not subscriber_uuid:
@@ -332,11 +424,21 @@ def confirm_optin(subscriber_uuid: str, list_uuid: str) -> bool:
 
 # endregion
 
-
 # region def update_subscriber(subscriber: models.Subscriber, add_to_lists: set[int], remove_from_lists: set[int])
 
 def update_subscriber(subscriber: models.Subscriber, add_to_lists: set[int] = None, remove_from_lists: set[int] = None,
                       status: SubscriberStatuses = SubscriberStatuses.enabled) -> models.Subscriber:
+    """
+    Update many aspects of a subscriber, from their email addresses and names, to custom attribute data, and
+    from adding them to and removing them from lists. You can enable, disable, and block them here. But if that
+    is all you want tod o there are functions dedicated to that which are simpler.
+    Args:
+        subscriber: The full subscriber object to update (with changed fields and values)
+        add_to_lists: Any list to add to this subscriber to.
+        remove_from_lists: Any list to remove from this subscriber.
+        status: The status of the subscriber: enabled, disabled, blacklisted from SubscriberStatuses.
+    Returns: The updated view of the subscriber object from the server.
+    """
     global core_headers
     validate_state(url=True, user=True)
     if subscriber is None or not subscriber.id:
@@ -367,22 +469,63 @@ def update_subscriber(subscriber: models.Subscriber, add_to_lists: set[int] = No
 
 # endregion
 
+# region def disable_subscriber(subscriber: models.Subscriber) -> models.Subscriber
+
 def disable_subscriber(subscriber: models.Subscriber) -> models.Subscriber:
+    """
+    Set a subscriber's status to disabled.
+    Args:
+        subscriber: The subscriber to disable.
+    Returns: The updated subscriber object from the server.
+    """
     return update_subscriber(subscriber, status=SubscriberStatuses.disabled)
 
 
+# endregion
+
+# region def enable_subscriber(subscriber: models.Subscriber) -> models.Subscriber
+
 def enable_subscriber(subscriber: models.Subscriber) -> models.Subscriber:
+    """
+    Set a subscriber's status to enabled.
+    Args:
+        subscriber: The subscriber to enable.
+    Returns: The updated subscriber object from the server.
+    """
     return update_subscriber(subscriber, status=SubscriberStatuses.enabled)
 
 
+# endregion
+
+# region def block_subscriber(subscriber: models.Subscriber) -> models.Subscriber
+
 def block_subscriber(subscriber: models.Subscriber) -> models.Subscriber:
+    """
+    Add a subscriber to the blocklist, AKA unsubscribe them.
+    Args:
+        subscriber: The subscriber to block/unsubscribe.
+    Returns: The updated subscriber object from the server.
+    """
     return update_subscriber(subscriber, status=SubscriberStatuses.blocklisted)
 
+
+# endregion
 
 # region def delete_subscriber(email: Optional[str] = None, overriding_subscriber_id: Optional[int] = None) -> bool
 
 def send_transactional_email(subscriber_email: str, template_id: int, from_email: str, template_data: dict,
                              messenger_channel: str = 'email', content_type: str = 'markdown') -> bool:
+    """
+    Send a transactional email through Listmonk to the recipient.
+    Args:
+        subscriber_email: The email address to send the email to (they must be a subscriber of *some* list on your server).
+        template_id: The template ID to use for the email. It must be a "transactional" not campaign template.
+        from_email: The from address for the email. Must be a valid from address on your email sending provider (e.g. SendGrid).
+        template_data: A dictionary of merge parameters for the template, available in the template as {{ .Tx.Data.* }}.
+        messenger_channel: Default is "email", if you have SMS or some other channel, you can use it here.
+        content_type: Email format options include html, markdown, and plain.
+    Returns: True if the email send was successful, False otherwise. Errors may show up in the logs section of your Listmonk dashboard.
+    """
     global core_headers
     validate_state(url=True, user=True)
     subscriber_email = (subscriber_email or '').lower().strip()
@@ -405,17 +548,19 @@ def send_transactional_email(subscriber_email: str, template_id: int, from_email
         raw_data = resp.json()
         return raw_data.get('data')  # {'data': True}
     except Exception:
-        # print(e)
-        # print(resp.text)
+        # Maybe some logging here at some point.
         raise
 
 
 # endregion
 
-
 # region def is_healthy() -> bool
 
 def is_healthy() -> bool:
+    """
+    Checks that the token retrieved during login is still valid at your server.
+    Returns: True if the token is still valid, False otherwise.
+    """
     # noinspection PyBroadException
     try:
         validate_state(url=True, user=True)
@@ -434,6 +579,10 @@ def is_healthy() -> bool:
 
 # region def verify_login() -> bool
 def verify_login() -> bool:
+    """
+    Call to verify that the stored auth token is still valid.
+    Returns: True if the stored auth token is still value, False otherwise.
+    """
     return is_healthy()
 
 
@@ -442,10 +591,13 @@ def verify_login() -> bool:
 # region def validate_login(user_name, pw)
 
 def validate_login(user_name, pw):
+    """
+    Internal use only.
+    """
     if not user_name:
-        raise Exception("Username cannot be empty")
+        raise ValidationError("Username cannot be empty")
     if not pw:
-        raise Exception("Password cannot be empty")
+        raise ValidationError("Password cannot be empty")
 
 
 # endregion
@@ -453,10 +605,13 @@ def validate_login(user_name, pw):
 # region def validate_state(url=False, user=False)
 
 def validate_state(url=False, user=False):
+    """
+    Internal use only.
+    """
     if url and not url_base:
-        raise Exception("URL Base must be set to proceed.")
+        raise OperationNotAllowedError("URL Base must be set to proceed.")
 
     if user and core_headers.get('Authorization') is None:
-        raise Exception("You must login before proceeding.")
+        raise OperationNotAllowedError("You must login before proceeding.")
 
 # endregion
