bb023f6175bcda2ff12dd834f67686d491de6598
Michael Kennedy <michael@talkpython.fm>
2024-10-28 17:51:36 -0700

    Update to work with listmonk 4.0.

--- listmonk/impl/__init__.py
+++ listmonk/impl/__init__.py
@@ -9,7 +9,7 @@ import httpx
 
 from listmonk import models, urls  # noqa: F401
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from listmonk.errors import ValidationError, OperationNotAllowedError, FileNotFoundError
 
