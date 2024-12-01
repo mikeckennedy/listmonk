ec3a401f09659dab6b4afd6237b9c5e1a3c90f48
Michael Kennedy <michael@talkpython.fm>
2024-12-01 13:46:42 -0800

    Need strenum dependency listed in BOTH places. :)

--- listmonk/impl/__init__.py
+++ listmonk/impl/__init__.py
@@ -10,7 +10,7 @@ import httpx
 
 from listmonk import models, urls  # noqa: F401
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from listmonk.errors import ValidationError, OperationNotAllowedError, FileNotFoundError
 
