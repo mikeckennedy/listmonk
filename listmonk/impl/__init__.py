9db73cd82eaefd7c65527a56fe1072ede4334149
Michael Kennedy <mikeckennedy@gmail.com>
2025-05-20 09:31:46 -0700

    Bump version to fix project definition. strenum wasn't included in the pyproject.toml's dependencies. No functional changes.

--- listmonk/impl/__init__.py
+++ listmonk/impl/__init__.py
@@ -9,7 +9,7 @@ import httpx
 
 from listmonk import models, urls  # noqa: F401
 
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 
 from listmonk.errors import ValidationError, OperationNotAllowedError, FileNotFoundError
 
