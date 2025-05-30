6b8f6e22d117444226aaa22fa1f0bd5296d75425
Michael Kennedy <mikeckennedy@gmail.com>
2025-05-29 23:35:48 -0700

    Bump version for next release.

--- listmonk/impl/__init__.py
+++ listmonk/impl/__init__.py
@@ -9,7 +9,7 @@ import httpx
 
 from listmonk import models, urls  # noqa: F401
 
-__version__ = '0.3.7'
+__version__ = '0.3.8'
 
 from listmonk.errors import ValidationError, OperationNotAllowedError, FileNotFoundError
 
